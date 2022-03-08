0250201d9afcaeea7d6f9e91d73a458ddb37915f
baldomarco <92460568+baldomarco@users.noreply.github.com>
2022-03-08 20:13:26 +0100

    Update 20220303_transition_period.r

--- 2022/20220303_transition_period.r
+++ 2022/20220303_transition_period.r
@@ -35,7 +35,7 @@ dynamicStand_cc <- dbReadTable(db1, "dynamicstand")
 
 dbDisconnect(db1)    # close the file
 
-carbonflow_cc <- dbReadTable(db2, "carbonflow")
+carbonflow_sw <- dbReadTable(db2, "carbonflow")
 carbon_sw <- dbReadTable(db2,"carbon")
 # wind <- dbReadTable(db2,"wind")
 # barkbeetle <- dbReadTable(db2,"barkbeetle")
@@ -84,7 +84,7 @@ ggplot(landscape_cc, aes(year,LAI))+
 
 ggplot(data=landscape_cc, aes(x=year, y=total_carbon_kg, color=species)) + 
   geom_line()+
-  ggtitle("Total Carbon per species time series in Clear cut")+
+  ggtitle("Total Carbon per species time series in Clear cut") +
   theme(plot.title = element_text(hjust = 0.5))
 
 #create pie chart
@@ -115,7 +115,7 @@ ggplot(landscape_cc, aes(x="", y=volume_m3, fill=species)) +
   theme(axis.line = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
- # to work you have add all the color for variable...-> scale_fill_manual(values=c("#FF5733", "#75FF33", "#33DBFF", "#BD33FF"))+
+  # to work you have add all the color for variable...-> scale_fill_manual(values=c("#FF5733", "#75FF33", "#33DBFF", "#BD33FF"))+
   ggtitle("Pie plot of avarage of 400y of landscape volume (m3) per species in Clear cut")
 
 # Make a plot with ggplot Basal Area by species (in landscape output), for the transitional period
@@ -147,27 +147,81 @@ ggplot(landscape_sw, aes(year,volume_m3, fill=species))+
   ggtitle("Shelterwood Volume Transitional Period by species")+
   theme(plot.title = element_text(hjust = 0.5))
 
-
-ggplot(landscape_sw, aes(year,volume_m3))+
+ggplot(abeUnit_sw, aes(year,volume))+
   geom_line(color = "steelblue") +
-  ggtitle("Shelterwood Volume Transitional Period prob. most common sp.")+
+  ggtitle("SW Volume Transitional Period")+
   theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(abeUnit_sw, aes(year,realizedHarvest))+
+  geom_line(color = "steelblue") +
+  ggtitle("SW Realized Harvest Transitional Period")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(carbonflow_sw, aes(year,NPP))+
+  geom_line(color = "steelblue") +
+  ggtitle("SW NPP Transitional Period")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(landscape_sw, aes(year,LAI))+
+  geom_area(color = "steelblue") +
+  ggtitle("SW LAI Transitional Period")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+# x <- c(carbonflow_cc$GPP, carbonflow_cc$NPP, carbonflow_cc$Rh, carbonflow_cc$NEP, carbonflow_cc$cumNPP, carbonflow_cc$cumRh, carbonflow_cc$cumNEP)
+
+# make the species C kg time series single line per species
+
+ggplot(data=landscape_sw, aes(x=year, y=total_carbon_kg, color=species)) + 
+  geom_line()+
+  ggtitle("Total Carbon per species time series in Shalterwood") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#create pie chart
+ggplot(landscape_sw, aes(x="", y=volume_m3, fill=species)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)+
+  ggtitle("Pie chart 400y avarage of landscape volume (m3) per species in Shlterwood")
+
+ggplot(landscape_sw, aes(x="", y=volume_m3, fill=species)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  theme_void()+
+  ggtitle("Pie chart 400y avarage of landscape volume (m3) per species in Shalterwood")
+
+ggplot(landscape_sw, aes(x="", y=volume_m3, fill=species)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  geom_text(aes(label = paste0(volume_m3, "%")), position = position_stack(vjust=0.5)) +
+  labs(x = NULL, y = NULL, fill = NULL)+
+  ggtitle("Pie chart 400 y avarage of landscape volume (m3) per species in Shalterwood")
+
+ggplot(landscape_sw, aes(x="", y=volume_m3, fill=species)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  geom_text(aes(label = paste0(volume_m3, "%")), position = position_stack(vjust=0.5)) +
+  labs(x = NULL, y = NULL, fill = NULL) +
+  theme_classic() +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank()) +
+  # to work you have add all the color for variable...-> scale_fill_manual(values=c("#FF5733", "#75FF33", "#33DBFF", "#BD33FF"))+
+  ggtitle("Pie plot of avarage of 400y of landscape volume (m3) per species in Clear cut")
 
 # Make a plot with ggplot Basal Area by species (in landscape output), for the transitional period
 
 ggplot(landscape_sw, aes(year,basal_area_m2, fill=species))+
   geom_area() +
-  ggtitle("Shelterwood Volume Transitional Period by species")+
+  ggtitle("Shalterwood Basal Area Transitional Period by species")+
   theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(landscape_sw, aes(year,basal_area_m2))+
-  geom_line(color = "steelblue") +
-  ggtitle("Shelterwood Volume Transitional Period prob. most common species")+
+ggplot(data=landscape_sw, aes(x=year, y=basal_area_m2, color=species)) + 
+  geom_line() +
+  ggtitle("Basal Area per species time series in shalterwood")+
   theme(plot.title = element_text(hjust = 0.5))
 
 
 # create the histogram of Volume/ DBH by species
-library(dplyr)
+# library(dplyr)
 
 #Clear cut
 
@@ -177,9 +231,15 @@ ggplot(landscape_cc, aes(year,dbh_avg_cm, fill=species))+
   theme(plot.title = element_text(hjust = 0.5))
 
 
-ggplot(landscape_cc, aes(year,dbh_avg_cm))+
-  geom_line(color = "steelblue") +
-  ggtitle("Clear Cut DBH Transitional Period")+
+# work with the filter to separate the species
+
+#ggplot(dynamicStand_cc, aes(year, dbh_mean, fill=species)) +
+#  geom_point() +
+#  stat_smooth()
+
+ggplot(data=dynamicStand_cc, aes(x=year,y=dbh_mean, color=species))+
+  geom_line() +
+  ggtitle("DBH mean Transitional Period by species in Clear Cut")+
   theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -191,9 +251,14 @@ ggplot(landscape_sw, aes(year,dbh_avg_cm, fill=species))+
   theme(plot.title = element_text(hjust = 0.5))
 
 
-ggplot(landscape_sw, aes(year,dbh_avg_cm))+
-  geom_line(color = "steelblue") +
-  ggtitle("Shelterwood DBH Transitional Period")+
+#ggplot(landscape_sw, aes(year,dbh_avg_cm))+
+#  geom_line(color = "steelblue") +
+#  ggtitle("Shelterwood DBH Transitional Period")+
+#  theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(data=dynamicStand_cc, aes(x=year,y=dbh_mean, color=species))+
+  geom_line() +
+  ggtitle("DBH mean Transitional Period by species in Clear Cut")+
   theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -203,7 +268,8 @@ hist(landscape_cc$year, landscape_cc$dbh_avg_cm, main= 'Clear Cut DBH Transition
 hist(landscape_sw$year, landscape_sw$dbh_avg_cm, main= 'Shalterwood DBH Transitional Period')
 
 ########################  Relation between DBH and Volume in the transition period
-#Clear cut
+
+# Clear cut
 
 ggplot(landscape_cc, aes(volume_m3,dbh_avg_cm, fill=species))+
   geom_area() +
@@ -231,9 +297,40 @@ ggplot(landscape_sw, aes(volume_m3,dbh_avg_cm))+
   theme(plot.title = element_text(hjust = 0.5))
 
 
-hist(landscape_cc$volume_m3, landscape_cc$dbh_avg_cm, main= 'Clear Cut V-DBH Transitional Period')
+hist(dynamicStand_cc$dbh_mean, main= 'Clear Cut V-DBH Transitional Period')
 
-hist(landscape_sw$volume_m3, landscape_sw$dbh_avg_cm, main= 'Shalterwood V-DBH Transitional Period')
+hist(dynamicstand_sw$dbh_mean, abeUnit_sw$volume, main= 'Shalterwood Vol-DBH Transitional Period')
+
+
+#________________________________________________________________________________________________________________
+
+ggplot(dynamicStand_cc, aes(year, if_dbh_40_and_dbh_60_volume_0_sum, fill=species))+
+  geom_area() +
+  ggtitle("Clear cut Volume based on DBH class 20-40cm Transitional Period by species")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+ggplot(dynamicstand_sw, aes(year, if_dbh_40_and_dbh_60_volume_0_sum, fill=species))+
+  geom_area() +
+  ggtitle("Shelterwood Volume based on DBH class 20-40cm Transitional Period by species")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+
+ggplot(data=dynamicstand_sw, aes(x=year, y= _if_dbh_20_and_dbh_40_volume_0_sum, color=species)) + 
+  geom_line() +
+  ggtitle("Basal Area per species time series in shalterwood")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+plot(dynamicstand_sw$year, dynamicstand_sw$if_dbh_40_and_dbh_60_volume_0_sum)
+
+
+
+
+
+
+
 
 # Report 
 
