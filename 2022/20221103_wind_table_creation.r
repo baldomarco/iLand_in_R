1117563517fae696e487ffbddf18e1a22fcee64b
baldomarco <92460568+baldomarco@users.noreply.github.com>
2022-11-03 19:38:04 +0100

    Update 20221103_wind_table_creation.r

--- 2022/20221103_wind_table_creation.r
+++ 2022/20221103_wind_table_creation.r
@@ -1,8 +1,6 @@
-#------ The real code for the wind table  ------------#
+#-------------------------------------------WIND TABLE AUTOMATISED CREATION IN R FOR ILAND WIND EVENT AND DISTURBANCE WIND MODULE-------------------------------#
 
 dataroot<-"C:/iLand/wind/random_wind_event_new_script/"
-
-# a<-read.table(paste0(dataroot,winds[i],"_", scens[j],".txt"), header=T)   # read the text file on my wind text
 
 a<-read.table(paste0(dataroot,"wind10.txt"), header=T)
 
@@ -20,11 +18,8 @@ year <- sample(1:300, 70, replace=F)
 
 year <- sort(year, decreasing=FALSE)
 
-# events<-which(a$year==20|a$year==50|a$year==80|a$year==110|a$year==140|a$year==170)  # which year I want and in specific years
-
 events<-which(a$year==4|a$year==6|a$year==9|a$year==10|a$year==13|a$year==16|a$year==17|a$year==19|a$year==25|a$year==34|a$year==35|a$year==40|a$year==46|a$year==47|a$year==62|a$year==65|a$year==66|a$year==69|a$year==77|a$year==78|a$year==83|a$year==89|a$year==90|a$year==97|a$year==99|a$year==100|a$year==102|a$year==115|a$year==120|a$year==123|a$year==124|a$year==128|a$year==133|a$year==136|a$year==137|a$year==152|a$year==154|a$year==162|a$year==169|a$year==172|a$year==178|a$year==180|a$year==188|a$year==189|a$year==190|a$year==192|a$year==199|a$year==200|a$year==204|a$year==208|a$year==214|a$year==217|a$year==218|a$year==225|a$year==232|a$year==247|a$year==248|a$year==249|a$year==257|a$year==261|a$year==264|a$year==266|a$year==270|a$year==273|a$year==276|a$year==278|a$year==284|a$year==285|a$year==291|a$year==298)
 
-#events<-which(a$year==year)
 
 events
 
@@ -36,12 +31,6 @@ a$modules.wind.dayOfYear[events]<-round(runif(70, 1, 365), 0)  # day of the year
 
 write.table(a,paste0(dataroot,"wind.txt"), col.names = T, quote=F, row.names=F, sep=" ")
 
-# write.table(a,paste0(dataroot,winds[i],"_",scens[j],"_upto2200.txt"), col.names = T, quote=F,row.names=F, sep=" ")  # write the table
-
-
-}
-
-}
 
 # the sample can be used to select the years randomly, just set "replace=F"  not to have more events in the same year
 # the sample function is good for the time randomization (give to us random numbers in a range together only integers)
